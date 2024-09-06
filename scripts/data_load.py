740c6fadae42a3eefa693eafb8b5702b9f4bab88
Ephrem Mekonnen <ephrem8118@gmail.com>
2024-09-06 10:30:17 +0300

    notebook user overview

--- scripts/data_load.py
+++ scripts/data_load.py
@@ -2,6 +2,7 @@ import os
 import psycopg2
 import pandas as pd
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 load_dotenv() # Load environment variables from .env file
 
@@ -12,21 +13,38 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-def load_data_from_postgres(query):
+
+def load_data_using_sqlalchemy(query):
 
     try:
-        connection = psycopg2.connect(
-            host=DB_HOST,
-            port=DB_PORT,
-            database=DB_NAME,
-            user=DB_USER,
-            password=DB_PASSWORD
-        )
-        
-        df = pd.read_sql_query(query, connection)
-        connection.close()
+        # Create a connection string
+        connection_string = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+        # Create an SQLAlchemy engine
+        engine = create_engine(connection_string)
+
+        # Load data into a pandas DataFrame
+        df = pd.read_sql_query(query, engine)
+
         return df
 
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
+
+
+def calculate_missing_percentage(df):
+    # Calculate the number of missing values in each column
+    missing_count = df.isnull().sum()
+    
+    # Calculate the percentage of missing values
+    missing_percentage = (missing_count / len(df)) * 100
+    
+    # Create a DataFrame to display the results
+    missing_summary = pd.DataFrame({
+        'Missing Values': missing_count,
+        'Percentage': missing_percentage
+    })
+    
+    return missing_summary
+
